829feb250f8f591311a651939d426df7389e5cce
sahasg96 <sahasg96@gmail.com>
2016-09-07 09:55:48 +0530

    Made variable type changes
    http://www.keil.com/support/man/docs/c51/c51_le_datatypes.htm
    
    List of data types supported

--- CoordinateToByte.h
+++ CoordinateToByte.h
@@ -1,15 +1,15 @@
-byte c2b( int coord[8][8], int row)
+unsigned char c2b( bit coord[8][8], int row)
 	{	
-		byte a = 0;
+		unsigned char a = 0;				// This is the temporary byte value which will be returned
 		for (int temp = 0; temp<=8; temp = temp +1)
 			{	
-				a = a*10;				
+				a>>1;				// left shift
 				a = a + coord[temp][row];    //adding all the values of the row together
 			}
 		return a;
 	}
 
-void c2b_all(int coord[8][8], byte data[])
+void c2b_all(bit coord[8][8], unsigned char data[])
 	{
 		for(int temp=0; temp<=8; temp = temp +1)
 			{
