dcb9f0e1c8f2c3136005cd95001605a4ac873b9f
sahasg96 <sahasg96@gmail.com>
2016-09-14 09:36:20 +0530

    edited the for loop

--- CoordinateToByte.h
+++ CoordinateToByte.h
@@ -11,7 +11,7 @@ unsigned char c2b( int coord[8][8], int row)
 
 void c2b_all(int coord[8][8], unsigned char data[])
 	{
-		for(int temp=0; temp<=8; temp = temp +1)
+		for(int temp=0; temp<8; temp = temp +1)
 			{
 				 data[temp] =  c2b(coord[8][8], temp);
 			}
