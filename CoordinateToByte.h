63aebbabe614b2745a9ee118acfdf65c4a52a26e
sahasg96 <sahasg96@gmail.com>
2016-09-14 09:32:37 +0530

    left shift

--- CoordinateToByte.h
+++ CoordinateToByte.h
@@ -3,7 +3,7 @@ unsigned char c2b( int coord[8][8], int row)
 		unsigned char a = 0;				// This is the temporary byte value which will be returned
 		for (int temp = 0; temp<=8; temp = temp +1)
 			{	
-				a = a>>1;				// left shift
+				a = a<<1;				// left shift
 				a = a + coord[temp][row];    //adding all the values of the row together
 			}
 		return a;
