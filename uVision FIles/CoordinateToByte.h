e4318af3f4f6701e14cde2a6da63273fc11322f4
sahasg96 <sahasg96@gmail.com>
2016-09-27 15:08:40 +0530

    changed array input size

--- uVision FIles/CoordinateToByte.h
+++ uVision FIles/CoordinateToByte.h
@@ -1,4 +1,4 @@
-unsigned char c2b( int coord[8][8], int row)
+unsigned char c2b( int coord[8][10], int row)
 	{	
 		unsigned char a = 0;				// This is the temporary byte value which will be returned
 		for (int temp = 0; temp<=8; temp = temp +1)
@@ -9,9 +9,9 @@ unsigned char c2b( int coord[8][8], int row)
 		return a;
 	}
 
-void c2b_all(int coord[8][8], unsigned char data[])
+void c2b_all(int coord[8][10], unsigned char data[])
 	{
-		for(int temp=0; temp<8; temp = temp +1)
+		for(int temp=0; temp<10; temp = temp +1)
 			{
 				 data[temp] =  c2b(coord[8][8], temp);
 			}
