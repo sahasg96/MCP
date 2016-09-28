55cd9ea818dcd1f22a265b7e8224d0117bcb6653
sahasg96 <sahasg96@gmail.com>
2016-09-28 09:10:50 +0530

    Clear Data

--- uVision FIles/UpdateCoordinate.h
+++ uVision FIles/UpdateCoordinate.h
@@ -1,18 +1,26 @@
-void UpdateBit (int x, int y, int value = 1, unsigned char coord[10][8])
+void UpdateBit (int x, int y, unsigned char coord[10][8])
 	{
-		coord[y][x] = value;
+		coord[y][x] = 1;
 	}
     
-void UpdateRow (int y, int value = 1, unsigned char coord[10][8])
+void UpdateRow (int y, unsigned char coord[10][8])
 	{
 		int x =0;
 		for(x=0;x<8;x++)
-		coord[y][x] = value;
+		coord[y][x] = 1;
 	}
 	
-void UpdateColoumn (int x, int value = 1, unsigned char coord[10][8])
+void UpdateColoumn (int x, unsigned char coord[10][8])
 	{
 		int y=0;
 		for(y=0;y<10;y++)
-		coord[y][x] = value;
+		coord[y][x] = 1;
+	}
+
+void ClearData(unsigned char coord[10][8])
+	{
+		int i,j;
+		for(i=0;i<10;i++)
+			for(j=0;j<8;j++)
+				coord[i][j] = 0;
 	}
