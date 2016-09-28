41b6bd9dcdd7d21d02d755179a5ac166e723ce2f
sahasg96 <sahasg96@gmail.com>
2016-09-28 09:06:14 +0530

    Default Value one

--- uVision FIles/UpdateCoordinate.h
+++ uVision FIles/UpdateCoordinate.h
@@ -1,16 +1,16 @@
-void UpdateBit (int x, int y, int value, unsigned char coord[10][8])
+void UpdateBit (int x, int y, int value = 1, unsigned char coord[10][8])
 	{
 		coord[y][x] = value;
 	}
     
-void UpdateRow (int y, int value, unsigned char coord[10][8])
+void UpdateRow (int y, int value = 1, unsigned char coord[10][8])
 	{
 		int x =0;
 		for(x=0;x<8;x++)
 		coord[y][x] = value;
 	}
 	
-void UpdateColoumn (int x, int value, unsigned char coord[10][8])
+void UpdateColoumn (int x, int value = 1, unsigned char coord[10][8])
 	{
 		int y=0;
 		for(y=0;y<10;y++)
