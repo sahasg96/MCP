07e333c0ba75d751d8b8b6226ab13a7f294d7825
sahasg96 <sahasg96@gmail.com>
2016-09-28 10:05:14 +0530

    int value added to all functions

--- uVision FIles/UpdateCoordinate.h
+++ uVision FIles/UpdateCoordinate.h
@@ -1,20 +1,20 @@
-void UpdateBit (int x, int y, unsigned char coord[10][8])
+void UpdateBit ( unsigned char coord[10][8], int x, int y, int value =1)
 	{
-		coord[y][x] = 1;
+		coord[y][x] = value;
 	}
     
-void UpdateRow (int y, unsigned char coord[10][8])
+void UpdateRow ( unsigned char coord[10][8],int y,int value =1)
 	{
 		int x =0;
 		for(x=0;x<8;x++)
-		coord[y][x] = 1;
+		coord[y][x] = value;
 	}
 	
-void UpdateColoumn (int x, unsigned char coord[10][8])
+void UpdateColoumn ( unsigned char coord[10][8], int x,int value =1)
 	{
 		int y=0;
 		for(y=0;y<10;y++)
-		coord[y][x] = 1;
+		coord[y][x] = value;
 	}
 
 void ClearData(unsigned char coord[10][8])
