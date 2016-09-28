3cb88236665b94aa79673b1508b1e0ca53bce999
sahasg96 <sahasg96@gmail.com>
2016-09-28 09:36:29 +0530

    Start byte data with 0

--- uVision FIles/MCP.c
+++ uVision FIles/MCP.c
@@ -3,16 +3,16 @@
 #include<CoordinateToByte.h>
 
 
-unsigned char bytedata [10] = { 0x01,
-																0x02,
-																0x04,
-																0x08,
-																0x10,
-																0x20,
-																0x40,
-																0x80,
-																0xaa,
-																0x55};
+unsigned char bytedata [10] = { 0x00,
+																0x00,
+																0x00,
+																0x00,
+																0x00,
+																0x00,
+																0x00,
+																0x00,
+																0x00,
+																0x00};
 
 unsigned char coord [10][8] = {
 				{0,0,0,0,0,0,0,1},
