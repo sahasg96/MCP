e27f657fffece3eb397eb929b649721d43d50e72
sahasg96 <sahasg96@gmail.com>
2016-10-05 10:29:49 +0530

    This is no longer needed
    Just edited the function definition, to make the changes lesser.

--- uVision FIles/CoordinateToByte.h
+++ uVision FIles/CoordinateToByte.h
@@ -1,20 +1,6 @@
-unsigned char c2b( unsigned char coord[10][8], int row)
-	{	
-		unsigned char a = 0;
-		int temp =0;// This is the temporary byte value which will be returned
-		for (temp = 0; temp<8; temp = temp +1)
-			{	
-				a = a<<1;				// left shift
-				a = a + coord[row][temp];    //adding all the values of the row together
-			}
-		return a;
-	}
-
-void c2b_all(unsigned char coord[10][8], unsigned char databyte[])
-	{	
-		int temp =0;
-		for( temp=0; temp<10; temp = temp +1)
-			{
-				 databyte[temp] =  c2b(coord, temp);
-			}
+void c2b_all(unsigned char coord[10], unsigned char databyte[10])
+	{
+		int i;
+		for(i=0;i<10;i++)
+			databyte[i] = coord[i];
 	}
