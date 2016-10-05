da055bb00938a8caa4e420a94809517b22aca8f8
sahasg96 <sahasg96@gmail.com>
2016-10-05 10:26:20 +0530

    more edits

--- uVision FIles/UpdateCoordinate.h
+++ uVision FIles/UpdateCoordinate.h
@@ -21,7 +21,50 @@ void UpdateRow (int y, bit value)
 
 void UpdateBit (int x, int y, bit value)
 	{
-		coord[y][x] = value;
+		if (value==1)
+			{
+					case(x)
+					{
+						0: coord[y] = coord[y]|0x01;
+							break;
+						1:coord[y] = coord[y]|0x02;
+							break;
+						2:coord[y] = coord[y]|0x04;
+							break;
+						3:coord[y] = coord[y]|0x08;
+							break;
+						4:coord[y] = coord[y]|0x10;
+							break;
+						5:coord[y] = coord[y]|0x20;
+							break;
+						6:coord[y] = coord[y]|0x40;
+							break;
+						7:coord[y] = coord[y]|0x80;
+							break;
+					}
+				}
+				else
+					{
+							case(x)
+							{
+								0: coord[y] = coord[y]|0xFE;
+									break;
+								1:coord[y] = coord[y]|0xFD;
+									break;
+								2:coord[y] = coord[y]|0xFB;
+									break;
+								3:coord[y] = coord[y]|0xF7;
+									break;
+								4:coord[y] = coord[y]|0xEF;
+									break;
+								5:coord[y] = coord[y]|0xDF;
+									break;
+								6:coord[y] = coord[y]|0xBF;
+									break;
+								7:coord[y] = coord[y]|0x7F;
+									break;
+							}
+						}
 	}
 
 
